d201e310a36ae3d962fcc299c945726b8799c940
H.E. van der Laan <h.e.v.d.laan@student.tue.nl>
2016-12-23 22:00:30 +0100

    Add title bar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+	def menu_link_to(text, path, controllername)
+		if controller.controller_name == controllername
+			content_tag(:li, link_to(text, path) + content_tag(:span, "(current)", class: "sr-only"), class: "active")
+		else
+			content_tag(:li, link_to(text, path))
+		end
+	end
 end
