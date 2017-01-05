04e0feb38a6b1988db62ef27f872e8c42f910684
H.E. van der Laan <h.e.v.d.laan@student.tue.nl>
2017-01-05 16:58:57 +0100

    Allow unlinking of accounts

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,4 +1,6 @@
 class AccountsController < ApplicationController
+	before_action :enforce_login, only: [:create, :destroy]
+
 	def list
 		@accounts = Account.all
 	end
@@ -20,10 +22,10 @@ class AccountsController < ApplicationController
 			flash[:danger] = "Could not link to that user: it doesn't exist"
 		elsif account.user
 			flash[:danger] = "Could not link to that user: " + account.name + " is already linked to " + account.user.name
-		elsif not logged_in?
-			flash[:danger] = "You need to log in to do that"
-			redirect_to login_path
-			return
+		#elsif not logged_in?
+		#	flash[:danger] = "You need to log in to do that"
+		#	redirect_to login_path
+		#	return
 		elsif Account.where(user: current_user, server: account.server).exists?
 			flash[:danger] = "You are already linked to an user on that server"
 		else
@@ -36,7 +38,27 @@ class AccountsController < ApplicationController
 		redirect_to action: 'new'
 	end
 
-	#def destroy
-		
-	#end
+	def destroy
+		account = Account.find_by_id(params["id"])
+		if account.user.nil?
+			flash[:danger] = "Could not unlink that user: it is not linked"
+		elsif account.user != current_user
+			flash[:danger] = "Could not unlink that user: it is not linked to you"
+		else
+			account.user = nil
+			account.save!
+			flash[:success] = "You are no longer linked to " + account.name
+		end
+		redirect_to current_user
+
+	end
+
+	private
+
+		def enforce_login
+			unless logged_in?
+				flash[:danger] = "You need to log in to do that"
+				redirect_to login_url
+			end
+		end
 end
