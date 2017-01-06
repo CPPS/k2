a996cc587a36876c608e5399e68efcc5c6ef0278
H.E. van der Laan <h.e.v.d.laan@student.tue.nl>
2017-01-06 11:22:40 +0100

    Fix problems page

--- app/helpers/problems_helper.rb
+++ app/helpers/problems_helper.rb
@@ -6,4 +6,18 @@ module ProblemsHelper
 		end
 		server.url + "problem.php?id=" + problem.problem_id
 	end
+
+	def get_all_submission_counts()
+		total_subs = Submission.select("problem_id, count(distinct account_id) as total").group("problem_id")
+		@count_submissions = {}
+		total_subs.each { |s| @count_submissions[s.problem_id] = s.total }
+		return @count_submissions
+	end
+
+	def get_all_accepted_submission_counts()
+		total_accepted_subs = Submission.select("problem_id, count(distinct account_id) as total").where(accepted: true).group("problem_id")
+		@count_accepted_submissions = {}
+		total_accepted_subs.each { |s| @count_accepted_submissions[s.problem_id] = s.total }
+		return @count_accepted_submissions
+	end
 end
