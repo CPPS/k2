da26d31dac8e5f8897b1d2465ff8cf1f710806c7
H.E. van der Laan <h.e.v.d.laan@student.tue.nl>
2017-01-07 21:15:15 +0100

    Show users who have solved that problem in problems

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -6,6 +6,7 @@ class ProblemsController < ApplicationController
 	def show
 		@problem = Problem.find_by_server_id_and_short_name(params[:server_id], params[:short_name])
 		@server = Server.find_by_id(params[:server_id])
-		@submissions = Submission.where(problem: @problem)
+		@accepted = @problem.submissions.where(submissions: { accepted: true }).includes(:account).order(created_at: :asc)
+		@sub_count = @problem.submissions.group(:account_id).count
 	end
 end
--- app/models/problem.rb
+++ app/models/problem.rb
@@ -1,4 +1,5 @@
 class Problem < ApplicationRecord
 	belongs_to :server
 	has_many :submissions
+	has_many :accounts, through: :submissions
 end
