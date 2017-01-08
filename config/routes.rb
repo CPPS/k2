e9f0a168e851af7a177c39af2547bc09701ec6ad
H.E. van der Laan <h.e.v.d.laan@student.tue.nl>
2017-01-08 16:24:01 +0100

    Coverup for missing dashboard page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ require 'sidekiq/web'
 
 Rails.application.routes.draw do
 	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-	root	'sessions#new'
+	root	'users#index'
 
 	get	'/login',	to: 'sessions#new'
 	post	'/login',	to: 'sessions#create'
