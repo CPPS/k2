7833652244281578a13b5c390f49b1d6b565d5db
H.E. van der Laan <h.e.v.d.laan@student.tue.nl>
2017-01-18 17:16:39 +0100

    Allow updating users without resetting password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,24 +1,27 @@
+# Models a user of our application
+# A user can log in and is linked to accounts on servers
 class User < ApplicationRecord
 	has_many :accounts
 	has_many :servers, through: :account
 
-	before_save { self.email.downcase! }
-	
-	validates :name, 
-		presence: true
+	before_save { email.downcase! }
 
-	validates :username, 
-		presence: true, 
-		length: { minimum: 4 }, 
-		uniqueness: { case_sensitive: false}
+	validates :name,
+	          presence: true
 
-	validates :email, 
-		presence: true, 
-		length: { minimum: 5 }, #TODO: proper regex
-		uniqueness: { case_sensitive: false }
+	validates :username,
+	          presence: true,
+	          length: { minimum: 4 },
+	          uniqueness: { case_sensitive: false }
+
+	validates :email,
+	          presence: true,
+	          length: { minimum: 5 }, # TODO: proper regex
+	          uniqueness: { case_sensitive: false }
 
 	has_secure_password
-	validates :password, 
-		presence: true, 
-		length: { minimum: 6 }
+	validates :password,
+	          presence: true,
+	          allow_nil: true,
+	          length: { minimum: 6 }
 end
