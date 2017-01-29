a156bd4aef4e401cd5663f8fa21c6e2f55b12124
H.E. van der Laan <h.e.v.d.laan@student.tue.nl>
2017-01-29 22:53:55 +0100

    Hotfix: I overlooked a job

--- app/jobs/score_update_job.rb
+++ app/jobs/score_update_job.rb
@@ -8,10 +8,10 @@ class ScoreUpdateJob < ApplicationJob
 		for a in accounts
 #			oldSolvedProblems = a.solvedProblems
 #			oldScore = a.score
-			a.solvedProblems = Submission.select(:problem_id).distinct.where(account: a, accepted: true).count
+			a.solvedProblems = Submission.accepted.select(:problem_id).distinct.where(account: a).count
 
 			# Ensure the score is calculated correctly in cases where a user submits two problems in one minute
-			a.score = Submission.select(:problem_id, :score).distinct.where(account: a, accepted: true).pluck(:problem_id, :score).sum { |s| s[1] }
+			a.score = Submission.accepted.select(:problem_id, :score).distinct.where(account: a).pluck(:problem_id, :score).sum { |s| s[1] }
 			a.save!
 		end
 	end
