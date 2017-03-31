bed20050de2d33f0f4d4a0586758f808831daf57
H.E. van der Laan <h.e.v.d.laan@student.tue.nl>
2017-03-31 18:24:42 +0200

    Remove caching of the get_attempted function
    This causes solution data of servers past the first not to show up

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -6,15 +6,14 @@ module AccountsHelper
 	end
 
 	def get_attempted(server)
-		return @user_attempted if @user_attempted
-		@user_attempted = {}
+		user_attempted = {}
 		account = user_account(server)
 		if account
 			attempted = Submission.attempted.where(account: account).pluck(:problem_id)
 			solved = Submission.accepted.where(account: account).pluck(:problem_id)
-			attempted.each { |p| @user_attempted[p] = false }
-			solved.each { |p| @user_attempted[p] = true }
+			attempted.each { |p| user_attempted[p] = false }
+			solved.each { |p| user_attempted[p] = true }
 		end
-		@user_attempted
+		user_attempted
 	end
 end
