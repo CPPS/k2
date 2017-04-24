2d296dc702d0034ce0e49dda10c609c4948ed48d
H.E. van der Laan <h.e.v.d.laan@student.tue.nl>
2017-04-24 11:18:56 +0200

    Add basic tests for problem page

--- test/controllers/problems_controller_test.rb
+++ test/controllers/problems_controller_test.rb
@@ -4,4 +4,14 @@ class ProblemsControllerTest < ActionDispatch::IntegrationTest
 	# test "the truth" do
 	#   assert true
 	# end
+	
+	test 'should get index' do
+		get problems_url
+		assert_response :success
+	end
+
+	test 'should get show' do
+		get kaas_url(1, 'T1')
+		assert_response :success
+	end
 end
