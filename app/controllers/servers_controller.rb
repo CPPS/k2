e8ed1a441f45bab977a5b4d3449932303a5aee62
H.E. van der Laan <opensource@henkvdlaan.com>
2018-02-01 16:35:51 +0100

    Fix Mysql compat issue

--- app/controllers/servers_controller.rb
+++ app/controllers/servers_controller.rb
@@ -10,7 +10,7 @@ class ServersController < ApplicationController
 		@feed = @server.submissions.includes(:account, :problem).order(submission_id: :desc).references(:accounts, :problems).limit(10)
 		@problems = @server.problems.order(short_name: :asc)
 		@accounts = @server.accounts.order(
-			'"accounts"."solvedProblems" DESC NULLS LAST', '"accounts"."score" ASC'
+			'-"accounts"."solvedProblems" ASC', '"accounts"."score" ASC'
 		)
 	end
 
