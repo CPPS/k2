36360a37a2f82fb4c9f9fa599f8803d831167a6a
H.E. van der Laan <opensource@henkvdlaan.com>
2018-02-28 23:16:33 +0100

    This doesn't work anymore with http basic auth

--- config/initializers/database_authenticatable.rb
+++ config/initializers/database_authenticatable.rb
@@ -5,14 +5,13 @@ module Devise
 		# We need to override Devise's database authenticatable strategy
 		# to prevent it from activating on LDAP users.
 		class DatabaseAuthenticatable < Authenticatable
-			def valid?
-				request.headers['PATHINFO'] == '/auth' ||
-				params[:user] && params[:user][:login] &&
-					User.where(
-						username: params[:user][:login],
-						ldap_user: false
-					).exists? && super
-			end
+#			def valid?
+#				params[:user] && params[:user][:login] &&
+#					User.where(
+#						username: params[:user][:login],
+#						ldap_user: false
+#					).exists?) && super
+#			end
 		end
 	end
 end
