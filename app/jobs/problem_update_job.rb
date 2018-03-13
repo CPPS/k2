e019d88ea802ff7fa7c6a4490dfd0fe954bf86ab
H.E. van der Laan <opensource@henkvdlaan.com>
2018-03-13 11:29:49 +0100

    Fix domjudge attribute rename

--- app/jobs/problem_update_job.rb
+++ app/jobs/problem_update_job.rb
@@ -3,7 +3,7 @@ require 'json'
 
 class ProblemUpdateJob < ApplicationJob
 	queue_as :default
-	
+
 	# Update all problems know to k2
 	def perform(*args)
 		servers = Server.all
@@ -12,18 +12,19 @@ class ProblemUpdateJob < ApplicationJob
 				puts "Unsupported Judge Type: " + s.api_type
 				next
 			end
-			
+
 			data = ""
 			open(s.api_endpoint + "problems?cid=#{s.contest_id}") do |f|
 				data = JSON.parse(f.read)
 			end
 			data.each do |p|
 				problem = Problem.find_or_initialize_by(
-					problem_id: p["id"], 
-					server_id: s.id, 
-
+					problem_id: p['id'],
+					server_id: s.id
 				)
-				problem.short_name = p["shortname"] 
+				problem.short_name = p['shortname']
+				# Domjudge changed the spelling of short_name.
+				problem.short_name ||= p['short_name']
 				problem.name = p["name"]
 				problem.label = p['label']
 				problem.save!
