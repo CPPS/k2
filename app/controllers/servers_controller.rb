5bb2abf27523a1247ceb6fff7cdf58e8e43480cb
H.E. van der Laan <opensource@henkvdlaan.com>
2018-03-16 21:49:30 +0100

    Hide problems that are hidden from submission.

--- app/controllers/servers_controller.rb
+++ app/controllers/servers_controller.rb
@@ -9,6 +9,7 @@ class ServersController < ApplicationController
 		@server = Server.find(params[:id])
 		@feed = @server.submissions.includes(:account, :problem).order(submission_id: :desc).references(:accounts, :problems).limit(10)
 		@problems = @server.problems.order(short_name: :asc)
+		                   .where.not(short_name: nil)
 		# TODO: consider not hiding system accounts
 		@accounts = @server.accounts.where.not(score: nil).order(
 			solvedProblems: :desc, score: :asc
