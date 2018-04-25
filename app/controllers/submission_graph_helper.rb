2dc641e67de6dcf2ab51d226b22778985b8beb87
martijntimmer <martijn_timmermans@hotmail.nl>
2018-04-25 10:27:45 +0200

    Ability to see achievements on account page

---
+++ app/controllers/submission_graph_helper.rb
@@ -0,0 +1,39 @@
+module SubmissionGraphHelper
+  def create_graph(user)
+  		account = Account.find_by(user_id: user.id) # Assume user has only 1 domjudge acc
+  		submissions = account.submissions.group('yearweek(created_at)').count 
+  		achievements = user.achievements
+
+  		data_subs = []  	
+  		submissions.each do |s, k|
+  			year = s.to_s[0..3].to_i
+  			week =  [1, s.to_s[4..5].to_i, 52].sort[1]
+			data_subs.push([Date.commercial(year,week).to_time.to_i*1000, k])
+
+		end
+		
+		data_achiev = []
+		achievements.each do |a|
+			data_achiev.push({x: a.date_of_completion.to_time.to_i*1000, text: "#{a.descr}", 
+				title: "<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIpKt_ZyFy7nRmZuKKFam6JkMwNX0vZgffBvBRHgXclVbbvLgz' width='20' height='20'>" })
+		end
+
+		@chart = LazyHighCharts::HighChart.new('graph') do |f|
+		  f.title(text: "Submissions over the years")
+		  f.xAxis(type: 'datetime')
+		  f.series(id: 'test', data: data_subs, name: 'Submissions')
+		  f.series(type:'flags', onSeries: 'test', data: data_achiev, useHTML: true, y: -45)
+		  f.legend(enabled: false)
+		  
+		  f.yAxis(title: {text: "Number of submissions"})
+		  f.chart({defaultSeriesType: "line"})
+		end		
+
+		@chart_globals = LazyHighCharts::HighChartGlobals.new do |f|
+  			f.global(useUTC: true)  			
+		end
+
+		return { chart: @chart, chart_globals: @chart_globals }
+  end
+end
+
--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,38 +1,11 @@
 class SubmissionsController < ApplicationController
+  include SubmissionGraphHelper
+  
   def show
-  		account = Account.find_by(user_id: current_user.id)
-  		submissions = account.submissions.group('yearweek(created_at)').count 
-  		achievements = current_user.achievements
+  		res = create_graph(current_user)
 
-  		data_subs = []  	
-  		submissions.each do |s, k|
-  			year = s.to_s[0..3].to_i
-  			week =  [1, s.to_s[4..5].to_i, 52].sort[1]
-			data_subs.push([Date.commercial(year,week).to_time.to_i*1000, k])
-
-		end
-		
-		data_achiev = []
-		achievements.each do |a|
-			data_achiev.push({x: a.date_of_completion.to_time.to_i*1000, text: "#{a.descr}", 
-				title: "<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIpKt_ZyFy7nRmZuKKFam6JkMwNX0vZgffBvBRHgXclVbbvLgz' width='20' height='20'>" })
-		end
-
-		@chart = LazyHighCharts::HighChart.new('graph') do |f|
-		  f.title(text: "Submissions over the years")
-		  f.xAxis(type: 'datetime')
-		  f.series(id: 'test', data: data_subs, name: 'Submissions')
-		  f.series(type:'flags', onSeries: 'test', data: data_achiev, useHTML: true, y: -45)
-		  f.legend(enabled: false)
-		  
-		  f.yAxis(title: {text: "Number of submissions"})
-		  f.chart({defaultSeriesType: "line"})
-		end		
-
-		@chart_globals = LazyHighCharts::HighChartGlobals.new do |f|
-  			f.global(useUTC: true)  			
-		end
-
+		@chart = res[:chart]
+		@chart_globals = res[:chart_globals]
 		@user_attempts = Submission.joins(:account)
 				.where( accounts: {user_id: current_user })
 				.order(created_at: :desc)
