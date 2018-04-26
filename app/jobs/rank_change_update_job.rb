3f78d9598fd0a024345e6de00b1319dc63379b64
martijntimmer <vbarvinko@live.com>
2018-04-26 09:14:14 +0200

    better rank change update

--- app/jobs/rank_change_update_job.rb
+++ app/jobs/rank_change_update_job.rb
@@ -5,9 +5,10 @@ class RankChangeUpdateJob < ApplicationJob
 		solvedProblems: :desc, score: :asc)
 
 	accounts.each_with_index do |account, index|
-		if(index != account.prev_rank-1 and account.score != 0) 
+		if index != account.prev_rank-1
+			if account.score != 0
+				accounts.rank_changed_at = Time.now()
 			account.rank_delta = index-(prev_rank-1)
-			accounts.rank_changed_at = Time.now()
 			account.prev_rank = index+1
 			account.save!
 		end
