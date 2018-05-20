3a0a202010f7b6389dbd94ff07790bceed9a2849
Vlad <vbarvinko@live.com>
2018-05-20 17:29:41 +0200

    fix Achievements tab

--- app/controllers/achievements_controller.rb
+++ app/controllers/achievements_controller.rb
@@ -1,6 +1,7 @@
 class AchievementsController < ApplicationController
 	def show
 		@shield_files = ['\assets\shield_platinum', '\assets\shield_gold', '\assets\shield_silver', '\assets\shield_bronze']
+		#@achievement_data = AchievementDatum.all.where.not(kind: :first_to_solve).order(
 		@achievement_data = AchievementDatum.all.where.not(kind: 999).order(
 			tier: :desc, title: :asc
 		)
