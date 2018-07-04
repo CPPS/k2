be34bc3f177440739f794e2ca23c8d5ce905613b
martijntimmer <martijn_timmermans@hotmail.nl>
2018-07-04 09:38:17 +0200

    Another asset fix

--- app/controllers/achievements_controller.rb
+++ app/controllers/achievements_controller.rb
@@ -1,6 +1,6 @@
 class AchievementsController < ApplicationController
 	def show
-		@shield_files = ['shield_platinum', 'shield_gold', 'shield_silver', 'shield_bronze']
+		@shield_files = ['shield_platinum.png', 'shield_gold.png', 'shield_silver.png', 'shield_bronze.png']
 		#@achievement_data = AchievementDatum.all.where.not(kind: :first_to_solve).order(
 		@achievement_data = AchievementDatum.all.where.not(kind: :first_to_solve).order(
 			tier: :desc, title: :asc
